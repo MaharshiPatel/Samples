6f6e6299b9f16f134502c47ed0ce922a0ee4c69c
Maharshi <Maharshi11patel@gmail.com>
2023-08-02 09:46:04 -0700

    Update Class1.cs

--- CentralPackageManagementExample/src/ClassLibrary1/Class1.cs
+++ CentralPackageManagementExample/src/ClassLibrary1/Class1.cs
@@ -1,4 +1,10 @@
-﻿namespace ClassLibrary1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1;
 public class Class1
 {
 
